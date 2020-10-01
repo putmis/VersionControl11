010d6411d30721c76250c09a897ef71b2f2e99f0
Mihály Puter <mihaly.puter@stud.uni-corvinus.hu>
2020-10-01 19:15:51 +0200

    load

--- 4szem/4szem/Form1.cs
+++ 4szem/4szem/Form1.cs
@@ -12,9 +12,18 @@ namespace _4szem
 {
     public partial class Form1 : Form
     {
+
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
         }
     }
 }
