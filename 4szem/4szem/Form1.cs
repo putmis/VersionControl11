4a0583803c34c7dc9bb2b8aad6e66e73f6cbae1f
Mihály Puter <mihaly.puter@stud.uni-corvinus.hu>
2020-10-01 19:47:33 +0200

    try catch

--- 4szem/4szem/Form1.cs
+++ 4szem/4szem/Form1.cs
@@ -20,6 +20,9 @@ namespace _4szem
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
 
+        Excel.Application xlApp; 
+        Excel.Workbook xlWB; 
+        Excel.Worksheet xlSheet; 
         public Form1()
         {
             InitializeComponent();
@@ -30,5 +33,33 @@ namespace _4szem
         {
             Flats = context.Flats.ToList();
         }
+
+        
+            private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+                CreateTable();
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+
+            }
+            catch (Exception ex)
+            {
+
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+        
     }
 }
