8917c46bda50ca1c40120b99233c43a6a6afcd54
Mihály Puter <mihaly.puter@stud.uni-corvinus.hu>
2020-10-11 21:53:37 +0200

    elfelejtett commitok

---
+++ 5.het/5.het/Entities/PortfolioItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.het.Entities
+{
+    class PortfolioItem
+    {
+
+        public string Index { get; set; }
+        public decimal Volume { get; set; }
+    }
+}
--- 5.het/5.het/Form1.cs
+++ 5.het/5.het/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _5.het.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,11 +16,37 @@ namespace _5.het
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
 
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
+        }
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
         }
     }
 }
