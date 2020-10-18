e5e1dfe5c6b74e8cc42a9eba949f90fa025df817
Mihály Puter <mihaly.puter@stud.uni-corvinus.hu>
2020-10-18 21:54:29 +0200

    otos

--- week06/week06/Form1.cs
+++ week06/week06/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using week06.Entities;
 
 namespace week06
@@ -15,6 +16,7 @@ namespace week06
     {
         BindingList<RateData> rates = new BindingList<RateData>();
 
+        private string result;
         public Form1()
         {
             InitializeComponent();
@@ -32,11 +34,41 @@ namespace week06
             };
 
             var response = mnbService.GetExchangeRates(request);
-            var result = response.GetExchangeRatesResult;
+            result = response.GetExchangeRatesResult;
 
             
 
 
         }
+
+        private void xmlfunction()
+        {
+            // XML document létrehozása és az aktuális XML szöveg betöltése
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            // Végigmegünk a dokumentum fő elemének gyermekein
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                // Létrehozzuk az adatsort és rögtön hozzáadjuk a listához
+                // Mivel ez egy referencia típusú változó, megtehetjük, hogy előbb adjuk a listához és csak később töltjük fel a tulajdonságait
+                var rate = new RateData();
+                rates.Add(rate);
+
+                // Dátum
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                // Valuta
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                // Érték
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
+        }
+
     }
 }
