987f98f17efe3873b43a5b92029562df3966bfb9
Mihály Puter <mihaly.puter@stud.uni-corvinus.hu>
2020-10-25 19:02:02 +0100

    harmadik

---
+++ week07/week07/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week07.Entities
+{
+    class BirthProbability
+    {
+        public double P { get; set; }
+        public int BirthYear { get; set; }
+        public int NbrOfChildren { get; set; }
+    }
+}
---
+++ week07/week07/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week07.Entities
+{
+    class DeathProbability
+    {
+        public int BirthYear { get; set; }
+        public Gender Gender { get; set; }
+        public double P { get; set; }
+    }
+}
