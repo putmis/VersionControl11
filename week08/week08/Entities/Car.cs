fa0f430759a2ef462b801d354126b1a71a9c7721
Mihály Puter <mihaly.puter@stud.uni-corvinus.hu>
2020-11-15 19:49:11 +0100

    7.

---
+++ week08/week08/Entities/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using week08.Abstractions;
+
+namespace week08.Entities
+{
+    class Car
+    {
+    }
+}
